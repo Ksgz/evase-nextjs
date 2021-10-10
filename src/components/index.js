f5302351ff1e6b92e65cee6d9cd223fdf9b73a51
Rasib Helal <rasibhelal@gmail.com>
2021-10-10 23:33:35 +0100

    .

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import Action from './Action';
 import CtaButtons from './CtaButtons';
 import DocsMenu from './DocsMenu';
-import htmlMenu from './htmlMenu';
 import Footer from './Footer';
 import Header from './Header';
 import Layout from './Layout';
@@ -12,13 +11,12 @@ import SectionDocs from './SectionDocs';
 import SectionGrid from './SectionGrid';
 import SectionHero from './SectionHero';
 
-export { Action, CtaButtons, DocsMenu, htmlMenu, Footer, Header, Layout, Icon, SectionContent, SectionCta, SectionDocs, SectionGrid, SectionHero };
+export { Action, CtaButtons, DocsMenu, Footer, Header, Layout, Icon, SectionContent, SectionCta, SectionDocs, SectionGrid, SectionHero };
 
 export default {
     Action,
     CtaButtons,
     DocsMenu,
-    htmlMenu,
     Footer,
     Header,
     Layout,
