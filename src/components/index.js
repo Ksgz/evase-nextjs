826e7a3c63d56e9220e0539ac5a7f52dbe60b526
Rasib Helal <rasibhelal@gmail.com>
2021-10-10 22:57:03 +0100

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Action from './Action';
 import CtaButtons from './CtaButtons';
 import DocsMenu from './DocsMenu';
-import HtmlMenu from './htmlMenu';
+import htmlMenu from './htmlMenu';
 import Footer from './Footer';
 import Header from './Header';
 import Layout from './Layout';
@@ -12,13 +12,13 @@ import SectionDocs from './SectionDocs';
 import SectionGrid from './SectionGrid';
 import SectionHero from './SectionHero';
 
-export { Action, CtaButtons, DocsMenu, HtmlMenu, Footer, Header, Layout, Icon, SectionContent, SectionCta, SectionDocs, SectionGrid, SectionHero };
+export { Action, CtaButtons, DocsMenu, htmlMenu, Footer, Header, Layout, Icon, SectionContent, SectionCta, SectionDocs, SectionGrid, SectionHero };
 
 export default {
     Action,
     CtaButtons,
     DocsMenu,
-    HtmlMenu,
+    htmlMenu,
     Footer,
     Header,
     Layout,
